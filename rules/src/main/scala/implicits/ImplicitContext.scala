a80e505ab94ac6fa6c4162a3f8ac624f371e46b5
Borja Lorente <blorente@ucm.es>
2017-10-09 18:36:11 +0200

    Begin fixing rule

--- rules/src/main/scala/implicits/ImplicitContext.scala
+++ rules/src/main/scala/implicits/ImplicitContext.scala
@@ -1,37 +1,26 @@
 package implicits
 
-import scalafix._
 import scala.meta._
 import scala.meta.contrib._
+import scalafix.lint.{LintCategory, LintSeverity}
+import scalafix.syntax._
+import scalafix.rule.RuleCtx
+import scalafix.{LintMessage, Patch, SemanticRule, SemanticdbIndex}
+
 
 final case class ImplicitContext(index: SemanticdbIndex)
   extends SemanticRule(index, "ImplicitContext")  {
 
  
   override def fix(ctx: RuleCtx): Patch = {
-    //ctx.debugIndex()
+    ctx.debugIndex()
     ctx.tree.collect {
       case x: Term.Apply => {
         println(s"Found Regular function call ${x.structure}")
       }
     }
-    ctx.index.database.synthetics.collect {
-      case x : Synthetic => {
-        val syntax = x.names(1).syntax.split(" ")
-        val name = syntax(1)
-        val callTemplate = ".*${name}(.*)".r
-        val isCall : Boolean = syntax(3) match {
-          case callTemplate() => {
-            println(s"Found Synthetic Method Call ${syntax(3)}")
-            true
-          }
-          case x => {
-            println(x)
-            false
-          }
-        }
-
-      }
+    for {synth <- ctx.index.database.synthetics} {
+      println(synth.names(1).symbol.denotation)
     }
     Patch.empty
   }
