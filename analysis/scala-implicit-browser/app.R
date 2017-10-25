95185229f64781f1f582f56549350699f618ec07
Filip Krikava <krikava@gmail.com>
2017-10-25 16:01:28 +0200

    Moved to MySQL

--- analysis/scala-implicit-browser/app.R
+++ analysis/scala-implicit-browser/app.R
@@ -5,25 +5,23 @@ library(visNetwork)
 library(igraph)
 library(stringr)
 library(janitor)
-library(RSQLite)
+library(RMySQL)
 library(dbplyr)
 
 NODE_PARAM <- 1L
 NODE_FUN <- 2L
 
-db_path <- "scala.sqlite3"
-
 load_data <- function() {
-  con <- DBI::dbConnect(RSQLite::SQLite(), db_path)
+  con <- DBI::dbConnect(RMySQL::MySQL(), dbname="scala", username="scala", password="scala", host="mysql")
   projects <- tbl(con, "projects") %>% collect(n=Inf)
   funs <- tbl(con, "funs")
   params <- tbl(con, "params")
   params_funs <- tbl(con, "params_funs")
-  
+
   list(
-    projects=projects, 
-    funs=funs, 
-    params=params, 
+    projects=projects,
+    funs=funs,
+    params=params,
     params_funs=params_funs,
     con=con
   )
@@ -31,18 +29,17 @@ load_data <- function() {
 
 create_model <- function(funs, params, params_funs) {
   # convert the ids to simple indexes
-  
+
   edges <- data_frame(
     from=match(params_funs$param, params$id),
-    # TODO: rename funapp -> fun
-    to=match(params_funs$funapp, funs$id) + nrow(params),
+    to=match(params_funs$fun, funs$id) + nrow(params),
     id=1:nrow(params_funs)
   )
 
   m_param_nodes <- params %>% transmute(
     id=1:nrow(params),
     node_type=NODE_PARAM,
-    
+
     name=name,
     fqn=fqn,
     type=type,
@@ -53,7 +50,7 @@ create_model <- function(funs, params, params_funs) {
   v_param_nodes <- m_param_nodes %>% transmute(
     id=1:nrow(params),
     node_type=NODE_PARAM,
-    
+
     group="parameters",
     shape="dot",
     title=str_c("<pre><code>implicit ", kind, " ", fqn, ".", name, ": ", fqtn, "</code></pre>", sep=""),
@@ -63,7 +60,7 @@ create_model <- function(funs, params, params_funs) {
   m_fun_nodes <- funs %>% transmute(
     id=nrow(params)+(1:nrow(funs)),
     node_type=NODE_FUN,
-    
+
     path=path,
     line=line,
     col=col,
@@ -85,20 +82,20 @@ create_model <- function(funs, params, params_funs) {
     title=str_c("<pre><code>", path, "\n", name, "</code></pre>"),
     color="lightblue"
   )
-  
+
   m_nodes <- bind_rows(m_param_nodes, m_fun_nodes)
   v_nodes <- bind_rows(v_param_nodes, v_fun_nodes)
-  
+
   list(
-    nodes=m_nodes, 
-    edges=edges, 
+    nodes=m_nodes,
+    edges=edges,
     graph=igraph::graph_from_data_frame(edges, vertices=v_nodes, directed=T)
   )
 }
 
 ui <- fluidPage(
    titlePanel("Implicit Browser"),
-   
+
    sidebarLayout(
       sidebarPanel(
         selectInput(
@@ -141,12 +138,12 @@ server <- function(input, output, session) {
     df <- data$projects %>%
       mutate(name=str_c(name, version, sep=":")) %>%
       select(name, id)
-    
+
     projects <- df$id
     names(projects) <- df$name
-    
+
     updateSelectInput(
-      session, 
+      session,
       "project",
       choices=projects,
       selected=projects[1]
@@ -156,33 +153,33 @@ server <- function(input, output, session) {
   model <- reactive({
     data <- data()
     p_id <- as.integer(input$project)
-    
+
     if (is.na(p_id)) {
       NULL
     } else {
-      funs <- 
-        data$funs %>% 
-        filter(project == p_id) %>% 
+      funs <-
+        data$funs %>%
+        filter(project == p_id) %>%
         collect(n=Inf)
-    
-      params <- 
-        data$params %>% 
-        filter(project == p_id) %>% 
+
+      params <-
+        data$params %>%
+        filter(project == p_id) %>%
         collect(n=Inf) %>%
         distinct(id, .keep_all=TRUE)
-      
+
       # there is a bug in DPLYR which generates wring query
-      params_funs <- 
+      params_funs <-
         tbl(
-          data$con, 
+          data$con,
           sql(str_c("select pf.* from params p inner join params_funs pf on p.id=pf.param where p.project=", p_id))
-        ) %>% 
+        ) %>%
         collect(n=Inf)
 
       create_model(funs, params, params_funs)
     }
   })
-  
+
   observe({
     model <- model()
     kind <- if (is.null(model)) {
@@ -194,7 +191,7 @@ server <- function(input, output, session) {
         distinct() %>%
         .$kind
     }
-    
+
     updateCheckboxGroupInput(
       session,
       "filter_kind",
@@ -202,7 +199,7 @@ server <- function(input, output, session) {
       selected=kind
     )
   })
-  
+
   observe({
     model <- model()
     modules <- if (is.null(model)) {
@@ -210,8 +207,8 @@ server <- function(input, output, session) {
     } else {
       model$nodes %>%
         filter(node_type == NODE_FUN) %>%
-        select(module) %>% 
-        distinct() %>% 
+        select(module) %>%
+        distinct() %>%
         .$module
     }
 
@@ -222,7 +219,7 @@ server <- function(input, output, session) {
       selected=modules
     )
   })
-  
+
   observe({
     model <- model()
     srcset <- if (is.null(model)) {
@@ -230,11 +227,11 @@ server <- function(input, output, session) {
     } else {
       model$nodes %>%
         filter(node_type == NODE_FUN) %>%
-        select(srcset) %>% 
-        distinct() %>% 
+        select(srcset) %>%
+        distinct() %>%
         .$srcset
     }
-    
+
     updateCheckboxGroupInput(
       session,
       "filter_srcset",
@@ -242,7 +239,7 @@ server <- function(input, output, session) {
       selected=srcset
     )
   })
-  
+
   graph <- reactive({
     model <- model()
     if (is.null(model)) {
@@ -252,7 +249,7 @@ server <- function(input, output, session) {
       param_nodes <-
         model$nodes %>%
         filter(node_type == NODE_PARAM & kind %in% input$filter_kind)
-  
+
       # filter funs
       fun_nodes <-
         model$nodes %>%
@@ -260,16 +257,16 @@ server <- function(input, output, session) {
           module %in% input$filter_module,
           srcset %in% input$filter_srcset
         )
-  
+
       eids <-
         model$edges %>%
         filter(from %in% param_nodes$id & to %in% fun_nodes$id) %>%
         .$id
-  
+
       subgraph.edges(model$graph, eids)
     }
   })
-   
+
   output$graph <- renderVisNetwork({
     graph <- graph()
     if (is.null(graph)) {
@@ -279,7 +276,7 @@ server <- function(input, output, session) {
       visOptions(nodesIdSelection=TRUE, highlightNearest=TRUE)
     }
   })
-  
+
   selected_node <- reactive({
     s_id = as.integer(input$graph_selected)
     if (!is.na(s_id) && is.integer(s_id) && length(s_id) == 1) {
@@ -288,7 +285,7 @@ server <- function(input, output, session) {
       NULL
     }
   })
-     
+
   output$node_details <- renderDataTable({
     s_id <- selected_node()
     if (is.null(s_id)) {
@@ -298,7 +295,7 @@ server <- function(input, output, session) {
       node %>% gather(na.rm=TRUE)
     }
   }, options=list("paging"=FALSE, "searching"=FALSE))
-   
+
   output$node_neighbors <- renderDataTable({
     s_id <- selected_node()
     if (is.null(s_id)) {
