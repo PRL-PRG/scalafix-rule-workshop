dca7d934f23d5c5639fce087ede9acdb3ae1bb58
Borja Lorente <blorente@ucm.es>
2017-11-03 10:54:09 +0100

    Refactor to fit fab commands

--- scripts/run.py
+++ scripts/run.py
@@ -11,10 +11,12 @@ from fabric.contrib import files
 import csv
 import subprocess
 from termcolor import colored
+import datetime
 
 def log(msg, color='magenta'):
+    timestamp = colored("{:%H:%M:%S.%f}".format(datetime.datetime.now()), 'yellow')
     label = colored("Runner", color)
-    print("[%s] %s" % (label, msg))
+    print("%s[%s]%s" % (timestamp, label, msg))
 
 def error(msg):
     log(msg, color='red')
@@ -40,7 +42,7 @@ def local_canfail(name, command, verbose=True, interactive=True):
                 if confirm("%s failed. Skip project?" % name):
                     failed = True
                 else:
-                    abort("Aborting at user request.")   
+                    abort("Aborting at user request.")
     return failed
 
 
@@ -50,7 +52,8 @@ def parse_cli_args():
     parser.add_argument('--cleanup', action='store_true', help='Invoke in cleanup mode. If invoked this way, only cleanup will be made')
     return parser.parse_args()
 
-def cleanup(cwd):
+def cleanup():
+    cwd = os.getcwd()
     log("Removing subdirs...")
     for item in os.listdir(cwd):
         if os.path.isdir(item):
@@ -65,124 +68,131 @@ def baseconfig():
     with open(os.path.join(base_path)) as base_file:
         return json.load(base_file)
 
-def load_config(file):
-    config = baseconfig()    
-    with open(file) as data_file:    
-        userconfig = json.load(data_file)
-        for field in userconfig:            
-            config[field] = userconfig[field]
+def override_config(base, userconfig):
+    config = base
+    for field in userconfig:
+        config[field] = userconfig[field]
     return config
 
-def import_projects(source, dest):
-    log("Importing projects into %s..." % dest)
-    for subdir in os.listdir(source):  
+def load_config(file):
+    config = baseconfig()
+    with open(file) as data_file:
+        userconfig = json.load(data_file)
+        return override_config(config, userconfig)
+
+def importp(source, dest="./projects"):
+    log("[Import] Importing projects into %s..." % dest)
+    for subdir in os.listdir(source):
         srcpath = os.path.join(source, subdir)
         destpath = os.path.join(dest, subdir)
         if os.path.isdir(srcpath) and os.path.exists(os.path.join(srcpath, "build.sbt")):
             try:
                 log("  %s" % str(srcpath))
                 if os.path.exists(destpath):
-                    log("    Already imported")
+                    log("[Import]   Already imported")
                 else:
-                    shutil.copytree(srcpath, destpath)  
-                    log("    Done!")              
+                    shutil.copytree(srcpath, destpath)
+                    log("[Import]   Done!")
             except RuntimeError as error:
-                print(error)   
+                print(error)
 
-def checkout_latest_tag():
-    log("      Checkout latest tag...")
-    local("git fetch --tags", capture=True)
-    local_canfail("Load latest tag", "latestTag=$( git describe --tags `git rev-list --tags --max-count=1` )", False, interactive=False)
-    local("git checkout $latestTag", capture=True)
 
-def create_project_info(name, path):
-    version = local("git describe --always", capture=True)
-    commit = local("git rev-parse HEAD", capture=True)
-    url = local("git config --get remote.origin.url", capture=True)
-    project_info = {"path": str(name), "name": str(name), "version": version, "last_commit": commit, "url": url}
-    log("      Capture project info: %s" % project_info)
-    with open(os.path.join(path, "project.csv"), "w") as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        writer.writerow(project_info.keys())
-        writer.writerow(project_info.values())
-    return project_info
+def analyze(subdir, config={}):
+    def checkout_latest_tag(project_name):
+        log("[%s]     Checkout latest tag..." % project_name)
+        local("git fetch --tags", capture=True)
+        local_canfail("Load latest tag", "latestTag=$( git describe --tags `git rev-list --tags --max-count=1` )", False, False)
+        local("git checkout $latestTag", capture=True)
 
-def download_sbt_plugin(plugin_url, dest_folder):    
-    if not os.path.exists(os.path.join(dest_folder, "SemanticdbConfigure.scala")):
-        with lcd(dest_folder):
-            local("wget -O SemanticdbConfigure.scala %s" % plugin_url, capture=True)
+    def create_project_info(name, path):
+        version = local("git describe --always", capture=True)
+        commit = local("git rev-parse HEAD", capture=True)
+        url = local("git config --get remote.origin.url", capture=True)
+        project_info = {"path": str(name), "name": str(name), "version": version, "last_commit": commit, "url": url}
+        log("[%s]     Capture project info: %s" % (name, project_info))
+        with open(os.path.join(path, "project.csv"), "w") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            writer.writerow(project_info.keys())
+            writer.writerow(project_info.values())
+        return project_info
 
-def generate_semanticdb_files(cwd, project_name, project_path, plugin_url):
-    log("  Looking for compilation report...")
-    continue_analysis = True
-    if not os.path.exists(os.path.join(project_path, "SEMANTICDB_COMPLILATION_COMPLETE.TXT")):
-        log("    Not found. Recompiling...")
-        with lcd(project_path):
-            checkout_latest_tag()
-            project_info = create_project_info(project_name, project_path)
-            download_sbt_plugin(plugin_url, "./project/")
-            failed = local_canfail("Generate semanticdb", "sbt semanticdb compile", False)   
-            if not failed: 
-                local("echo %s >> SEMANTICDB_COMPLILATION_COMPLETE.TXT" % project_info["version"])
+    def download_sbt_plugin(plugin_url, dest_folder):
+        if not os.path.exists(os.path.join(dest_folder, "SemanticdbConfigure.scala")):
+            with lcd(dest_folder):
+                local("wget -O SemanticdbConfigure.scala %s" % plugin_url, capture=True)
+
+    def generate_semanticdb_files(cwd, project_name, project_path, plugin_url):
+        log("[%s] Looking for compilation report..." % project_name)
+        continue_analysis = True
+        if not os.path.exists(os.path.join(project_path, "SEMANTICDB_COMPLILATION_COMPLETE.TXT")):
+            log("[%s]   Not found. Recompiling..." % project_name)
+            with lcd(project_path):
+                checkout_latest_tag(project_name)
+                project_info = create_project_info(project_name, project_path)
+                download_sbt_plugin(plugin_url, "./project/")
+                failed = local_canfail("Generate semanticdb", "sbt semanticdb compile", False)
+                if not failed:
+                    local("echo %s >> SEMANTICDB_COMPLILATION_COMPLETE.TXT" % project_info["version"])
+                else:
+                    error("[%s]   Skipping project" % project_name)
+                    continue_analysis = False
+            lcd(cwd)
+        else:
+            log("[%s]   Compilation report found. Skipping" % project_name)
+        return continue_analysis
+
+    def run_analysis_tool(project_name, project_path, tool_path, jvm_options):
+        log("[%s] Analyzing semanticdb files..." % project_name)
+        continue_analysis = True
+        if not os.path.exists(os.path.join(project_path, "SEMANTICDB_ANALYSIS_COMPLETE.TXT")):
+            failed = local_canfail("Semanticdb file analysis", "java -jar %s %s %s" % (jvm_options, tool_path, project_path))
+            if not failed:
+                local("cat %s/SEMANTICDB_COMPLILATION_COMPLETE.TXT >> %s/SEMANTICDB_ANALYSIS_COMPLETE.TXT" % (project_path, project_path))
+                log("[%s]   Done" % project_name)
             else:
-                error("    Skipping project")
+                error("[%s]   Skipping project" % project_name)
                 continue_analysis = False
-        lcd(cwd)
-    else:
-        log("    Compilation report found. Skipping")
-    return continue_analysis
-
-def run_analysis_tool(project_path, tool_path, jvm_options):
-    log("  Analyzing semanticdb files...")
-    continue_analysis = True
-    if not os.path.exists(os.path.join(project_path, "SEMANTICDB_ANALYSIS_COMPLETE.TXT")):        
-        failed = local_canfail("Semanticdb file analysis", "java -jar %s %s %s" % (jvm_options, tool_path, project_path))
-        if not failed:
-            local("cat %s/SEMANTICDB_COMPLILATION_COMPLETE.TXT >> %s/SEMANTICDB_ANALYSIS_COMPLETE.TXT" % (project_path, project_path))
-            log("    Done")
         else:
-            error("    Skipping project")
-            continue_analysis = False
-    else:
-        log("    Analysis report found. Skipping")
-    return continue_analysis
+            log("[%s]   Analysis report found. Skipping" % project_name)
+        return continue_analysis
 
-def run_cleanup_tool(project_path, tool_path):
-    log("  Cleaning up the data...")
-    continue_analysis = True
-    if not os.path.exists(os.path.join(project_path, "DATA_CLEANUP_COMPLETE.TXT")):        
-        failed = local_canfail("Data cleanup", "python %s %s" % (tool_path, project_path))
-        if not failed:
-            local("cat %s/SEMANTICDB_ANALYSIS_COMPLETE.TXT >> %s/DATA_CLEANUP_COMPLETE.TXT" % (project_path, project_path))
-            log("    Done")
-        else:
-            log("    Skipping project")
-            continue_analysis = False
-    else:
-        log("    Cleanup report found. Skipping")
-    return continue_analysis
-
-def upload_to_database(project_path, tool_path, commit_to_db):
-    log("  Uploading to database...(commit changes: %s)" % commit_to_db)
-    continue_analysis = True
-    commit_option = "-y" if commit_to_db else "-n"
-    if not os.path.exists(os.path.join(project_path, "DB_UPLOAD_COMPLETE.TXT")):        
-        failed = local_canfail("DB upload", "python %s %s %s" % (tool_path, commit_option, project_path), False)
-        if not failed:
-            local("cat %s/DATA_CLEANUP_COMPLETE.TXT >> %s/DB_UPLOAD_COMPLETE.TXT" % (project_path, project_path))
-            
-            if commit_to_db:
-                log("    Changes commited to the database", color='green')
+    def run_cleanup_tool(project_name, project_path, tool_path):
+        log("[%s] Cleaning up the data..." % project_name)
+        continue_analysis = True
+        if not os.path.exists(os.path.join(project_path, "DATA_CLEANUP_COMPLETE.TXT")):
+            failed = local_canfail("Data cleanup", "python %s %s" % (tool_path, project_path))
+            if not failed:
+                local("cat %s/SEMANTICDB_ANALYSIS_COMPLETE.TXT >> %s/DATA_CLEANUP_COMPLETE.TXT" % (project_path, project_path))
+                log("[%s]   Done" % project_name)
             else:
-                log("    DB changes NOT COMMITED", color='yellow')
+                log("[%s]   Skipping project" % project_name)
+                continue_analysis = False
         else:
-            log("    Skipping project")
-            continue_analysis = False
-    else:
-        log("    Upload report found. Skipping")
-    return continue_analysis
+            log("[%s]   Cleanup report found. Skipping" % project_name)
+        return continue_analysis
 
-def analyze_projects(cwd, config):        
+    def upload_to_database(project_name, project_path, tool_path, commit_to_db):
+        log("[%s] Uploading to database...(commit changes: %s)" % (project_name, commit_to_db))
+        continue_analysis = True
+        commit_option = "-y" if commit_to_db else "-n"
+        if not os.path.exists(os.path.join(project_path, "DB_UPLOAD_COMPLETE.TXT")):
+            failed = local_canfail("DB upload", "python %s %s %s" % (tool_path, commit_option, project_path), False)
+            if not failed:
+                local("cat %s/DATA_CLEANUP_COMPLETE.TXT >> %s/DB_UPLOAD_COMPLETE.TXT" % (project_path, project_path))
+                if commit_to_db:
+                    log("[%s]   Changes commited to the database" % project_name, color='green')
+                else:
+                    log("[%s]   DB changes NOT COMMITED" % project_name, color='yellow')
+            else:
+                log("[%s]   Skipping project" % project_name)
+                continue_analysis = False
+        else:
+            log("[%s]   Upload report found. Skipping" % project_name)
+        return continue_analysis
+
+    cwd = os.getcwd()
+    config = override_config(baseconfig(), config)
+
     projects = config["projects_dest"]
     plugin_url = config["semanticdb_plugin_url"]
     jvm_options = config["analyzer_jvm_options"]
@@ -190,27 +200,35 @@ def analyze_projects(cwd, config):
     cleanup_tool_path = os.path.join(cwd, config["tools_dir"], config["cleanup_tool_name"])
     db_tool_path = os.path.join(cwd, config["tools_dir"], config["db_push_tool_name"])
     projects_path = os.path.join(cwd, projects)
-    for subdir in os.listdir(projects_path):
-        log("%s" % subdir)
-        subdir_path = os.path.join(projects_path, subdir)
-        continue_analysis = generate_semanticdb_files(projects_path, subdir, subdir_path, plugin_url)
-        if continue_analysis: 
-            continue_analysis = run_analysis_tool(subdir_path, analysis_tool_path, jvm_options)
-        if continue_analysis:
-            continue_analysis = run_cleanup_tool(subdir_path, cleanup_tool_path)
-        if continue_analysis: 
-            continue_analysis = upload_to_database(subdir_path, db_tool_path, config["commit_to_db"])
 
-def download_extraction_tools(cwd, config):
+    log("[%s]" % subdir)
+    subdir_path = os.path.join(projects_path, subdir)
+    continue_analysis = generate_semanticdb_files(projects_path, subdir, subdir_path, plugin_url)
+    if continue_analysis:
+        continue_analysis = run_analysis_tool(subdir, subdir_path, analysis_tool_path, jvm_options)
+    if continue_analysis:
+        continue_analysis = run_cleanup_tool(subdir, subdir_path, cleanup_tool_path)
+    if continue_analysis:
+        continue_analysis = upload_to_database(subdir,subdir_path, db_tool_path, config["commit_to_db"])
+
+def analyze_projects(cwd, config):
+    projects = config["projects_dest"]
+    projects_path = os.path.join(cwd, projects)
+    for subdir in os.listdir(projects_path):
+        analyze(subdir)
+
+def setup(config={}):
+    cwd = os.getcwd()
+    config = override_config(baseconfig(), config)
     def download_tool(title, name, url):
-        log("  %s" % title)
+        log("[Setup] %s" % title)
         tool_path = os.path.join(tools_dir, name)
         if not os.path.exists(tool_path):
-            log("    Not found. Dowloading...")
+            log("[Setup]   Not found. Dowloading...")
             local("wget -O %s %s" % (name, url), capture=True)
         else:
-            log("    Already downloaded")
-        
+            log("[Setup]   Already downloaded")
+
     log("Downloading tools...")
     tools_dir = os.path.join(cwd, config["tools_dir"])
     if not os.path.exists(tools_dir):
@@ -223,8 +241,8 @@ def download_extraction_tools(cwd, config):
 
 
 def run(cwd, config):
-    import_projects(config["sbt_projects"], config["projects_dest"])
-    download_extraction_tools(cwd, config)
+    importp(config["sbt_projects"], config["projects_dest"])
+    setup(config)
     analyze_projects(cwd, config)
     log("All done!", color="green")
 
@@ -242,7 +260,3 @@ def main():
         log("No config provided. Loading default...")
         config = baseconfig()
         run(cwd, config)
-
-main()
-
-    
