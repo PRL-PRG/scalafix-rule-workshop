5e88e44e1a49466ebdbb96d667e66b4c1b26addb
Borja Lorente <blorente@ucm.es>
2017-11-13 09:22:59 +0100

    Change function dict for function composition

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -3,7 +3,7 @@ import subprocess
 import csv
 import sys
 import re
-from Fixer import Fixer
+import functools
 
 unknow_kind_pattern = r".*Denotation\(([A-Z]*( \| [A-Z]*)*).*"
 L_notation_pattern = re.compile(r"L([a-zA-Z-_$]*\/)*[a-zA-Z-_$]*;")
@@ -58,13 +58,15 @@ def fix(transforms, text):
         text = fixes[transform](text)
     return text
 
-fully_qualified_name_fixes = [
-    "remove_leading_root",
-    "remove_trailing_dot",
-    "extract_function_name",
-    "remove_L_notation",
-    "remove_hashtags",
-]
+def compose(*fs):
+    return functools.reduce(lambda f, g: lambda x: f(g(x)), fs, lambda x: x)
+
+clean_fqn = compose(
+    remove_leading_root,
+    remove_trailing_dot,
+    remove_L_notation,
+    remove_hashtags
+)
 
 def get_project_info(cwd):
     with open(os.path.join(cwd, "project.csv"), "r") as projfile:
@@ -75,30 +77,25 @@ def get_project_info(cwd):
         return info
 
 def clean_param_row(row):
-    row["fqn"] = fix(fully_qualified_name_fixes, row["fqn"])
-    row["class"] = fix(fully_qualified_name_fixes, row["class"])
-    row["kind"] = fix(["replace_unknown_kinds"], row["kind"])
+    row["fqn"] = extract_function_name(clean_fqn(row["fqn"]))
+    row["class"] = extract_function_name(clean_fqn(row["class"]))
+    row["kind"] = replace_unknown_kinds(row["kind"])
     return row
 
 def clean_funs_row(row):
-    row["symbol"] = fix(["remove_leading_root", "remove_trailing_dot", "remove_L_notation", "remove_hashtags"], row["symbol"])
-    row["fqfn"] = fix(["extract_function_name"], row["symbol"])
-    row["fqparamlist"] = fix(["extract_parameter_list"], row["symbol"])
+    row["symbol"] = clean_fqn(row["symbol"])
+    row["fqfn"] = extract_function_name(row["symbol"])
+    row["fqparamlist"] = extract_parameter_list(row["symbol"])
     return row
 
 def clean_links_row(row):
-    row["from"] = fix(fully_qualified_name_fixes, row["from"])
+    row["from"] = clean_fqn(row["from"])
     return row
 
 def clean_declared_implicits_row(row):
-    try:
-        row["fqn"] = fix(["remove_leading_root", "remove_trailing_dot", "remove_L_notation", "remove_hashtags"], row["fqn"])
-        row["kind"] = fix(["replace_unknown_kinds"], row["kind"])
-        row["class"] = fix(fully_qualified_name_fixes, row["class"])
-    except AttributeError as error:
-        print(row)
-        print(error)
-        sys.exit(1)
+    row["fqn"] = clean_fqn(row["fqn"])
+    row["kind"] = replace_unknown_kinds(row["kind"])
+    row["class"] = extract_function_name(clean_fqn(row["class"]))
     return row
 
 def clean_file(directory, filename, clean_function):
@@ -127,19 +124,19 @@ def clean_project(directory):
     clean_file(directory, "declared-implicits", clean_declared_implicits_row)
 
 def test():
-    assert(fix(["remove_leading_root", "remove_trailing_dot"], "_root_.implicits.ComplexArgument#WriterLike.WriterStringInt.")
+    assert(remove_leading_root(remove_trailing_dot("_root_.implicits.ComplexArgument#WriterLike.WriterStringInt."))
             == "implicits.ComplexArgument#WriterLike.WriterStringInt")
-    assert(fix(["remove_L_notation"], "_root_.org.scalacheck.util.Pretty.prettyAny(Ljava/lang/Object;)Lorg/scalacheck/util/Pretty;.")
+    assert(remove_L_notation("_root_.org.scalacheck.util.Pretty.prettyAny(Ljava/lang/Object;)Lorg/scalacheck/util/Pretty;.")
             == "_root_.org.scalacheck.util.Pretty.prettyAny(java.lang.Object)org.scalacheck.util.Pretty.")
-    assert(fix(["replace_unknown_kinds"], "<unknown: Denotation(IMPLICIT | MACRO, 'methodFor', '[T] => (f: Function0[T]): ScalaMethodCall[T]')>")
+    assert(replace_unknown_kinds("<unknown: Denotation(IMPLICIT | MACRO, 'methodFor', '[T] => (f: Function0[T]): ScalaMethodCall[T]')>")
             == "IMPLICIT | MACRO")
-    assert(fix(["extract_function_name"], "com.twitter.algebird.Monad.operators(java.lang.Objectcom.twitter.algebird.Monad)com.twitter.algebird.MonadOperators.[M]")
+    assert(extract_function_name("com.twitter.algebird.Monad.operators(java.lang.Objectcom.twitter.algebird.Monad)com.twitter.algebird.MonadOperators.[M]")
             == "com.twitter.algebird.Monad.operators")
-    assert(fix(["extract_parameter_list"], "com.twitter.algebird.Monad.operators(java.lang.Objectcom.twitter.algebird.Monad)com.twitter.algebird.MonadOperators.[M]")
+    assert(extract_parameter_list("com.twitter.algebird.Monad.operators(java.lang.Objectcom.twitter.algebird.Monad)com.twitter.algebird.MonadOperators.[M]")
             == "(java.lang.Objectcom.twitter.algebird.Monad)com.twitter.algebird.MonadOperators.[M]")
-    assert(fix(["extract_function_name"], "_root_.org.scalacheck.util.Pretty.prettyAny")
+    assert(extract_function_name("_root_.org.scalacheck.util.Pretty.prettyAny")
             == "_root_.org.scalacheck.util.Pretty.prettyAny")
-    assert(fix(["remove_L_notation"], "_root_.akka.actor.Scheduler#schedule(Lscala/concurrent/duration/FiniteDuration;Lscala/concurrent/duration/FiniteDuration;Lakka/actor/ActorRef;Ljava/lang/Object;Lscala/concurrent/ExecutionContext;Lakka/actor/ActorRef;)Lakka/actor/Cancellable;.")
+    assert(remove_L_notation("_root_.akka.actor.Scheduler#schedule(Lscala/concurrent/duration/FiniteDuration;Lscala/concurrent/duration/FiniteDuration;Lakka/actor/ActorRef;Ljava/lang/Object;Lscala/concurrent/ExecutionContext;Lakka/actor/ActorRef;)Lakka/actor/Cancellable;.")
             == "_root_.akka.actor.Scheduler#schedule(scala.concurrent.duration.FiniteDuration,scala.concurrent.duration.FiniteDuration,akka.actor.ActorRef,java.lang.Object,scala.concurrent.ExecutionContext,akka.actor.ActorRef)akka.actor.Cancellable.")
     print("All tests passed!")
 
