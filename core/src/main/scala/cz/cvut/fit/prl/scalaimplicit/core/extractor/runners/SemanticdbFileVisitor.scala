1a1cadf2e85da6cd60b65aa85f300513f8501f5f
Borja Lorente <blorente@ucm.es>
2018-01-16 13:06:02 -0500

    Add handling for warning messages

--- core/src/main/scala/cz/cvut/fit/prl/scalaimplicit/core/extractor/runners/SemanticdbFileVisitor.scala
+++ core/src/main/scala/cz/cvut/fit/prl/scalaimplicit/core/extractor/runners/SemanticdbFileVisitor.scala
@@ -7,16 +7,31 @@ import cz.cvut.fit.prl.scalaimplicit.core.extractor.{ExtractionResult, Result}
 import cz.cvut.fit.prl.scalaimplicit.core.extractor.contexts.SemanticCtx
 import cz.cvut.fit.prl.scalaimplicit.core.extractor.contexts.ReflectiveCtx
 import org.langmeta.internal.semanticdb.{schema => s}
+import org.langmeta.{semanticdb => d}
 
 import scala.util.control.NonFatal
+
+object DBOps {
+  // Transform the database from the internal format to the scala.meta format
+  private def toMetaDB(from: s.Database): d.Database = {
+    // Discard documents that have only warning messages
+    def discardMessageDocs(docs: Seq[s.Document]): Seq[s.Document] =
+      docs.filterNot(doc => doc.messages.nonEmpty && doc.contents.isEmpty)
+
+    from.copy(documents = discardMessageDocs(from.documents)).toDb(None)
+  }
+
+  def loadDB(filePath: Path): d.Database = {
+    toMetaDB(s.Database.parseFrom(Files.readAllBytes(filePath)))
+  }
+}
 
 object SemanticDBFileVisitor
     extends ((Path, (SemanticCtx => Result)) => Result)
     with LazyLogging {
   def apply(filePath: Path, f: SemanticCtx => Result): Result = {
     try {
-      val sdb = s.Database.parseFrom(Files.readAllBytes(filePath))
-      val mdb = sdb.toDb(None)
+      val mdb = DBOps.loadDB(filePath)
       val ctx = SemanticCtx(mdb)
       val res = f(ctx)
       logger.debug(s"Processing $filePath")
@@ -38,8 +53,7 @@ object ReflectiveVisitor
             loader: ClassLoader,
             f: ReflectiveCtx => ExtractionResult): ExtractionResult = {
     try {
-      val sdb = s.Database.parseFrom(Files.readAllBytes(filePath))
-      val mdb = sdb.toDb(None)
+      val mdb = DBOps.loadDB(filePath)
       val ctx = new ReflectiveCtx(loader, mdb)
       logger.debug(s"Processing $filePath")
       val res = f(ctx)
