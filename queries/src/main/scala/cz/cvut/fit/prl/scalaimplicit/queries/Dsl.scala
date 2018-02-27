ce5ab1ea11d50443c9dcc20d81d91b0e3e0db87e
Filip Krikava <krikava@gmail.com>
2018-02-27 23:48:20 +0100

    Updated non-type safe version (WIP)

--- queries/src/main/scala/cz/cvut/fit/prl/scalaimplicit/queries/Dsl.scala
+++ queries/src/main/scala/cz/cvut/fit/prl/scalaimplicit/queries/Dsl.scala
@@ -2,9 +2,9 @@ package cz.cvut.fit.prl.scalaimplicit.queries
 
 import cats._
 import cats.implicits._
+import com.sun.org.glassfish.gmbal.Description
 import cz.cvut.fit.prl.scalaimplicit.core.extractor.representation.Representation
-import cz.cvut.fit.prl.scalaimplicit.core.extractor.representation.Representation.CallSite
-import cz.cvut.fit.prl.scalaimplicit.core.extractor.representation.Representation.Type
+import cz.cvut.fit.prl.scalaimplicit.core.extractor.representation.Representation.{CallSite, Declaration, Location, Type}
 
 import scala.collection.TraversableLike
 import scala.collection.generic.CanBuildFrom
@@ -38,10 +38,10 @@ case class Mismatch[A](instance: A, matcher: Matcher[A]) extends MatchResult[A] 
   override def reason: String = matcher.describeMismatch(instance).get
 }
 
-trait Matcher[A] extends (A => MatchResult[A]) {
+trait Matcher[-A] {
   self =>
 
-  def matches(v: A): Boolean
+  def test(v: A): Boolean
 
   def description: String
 
@@ -51,25 +51,25 @@ trait Matcher[A] extends (A => MatchResult[A]) {
 
   def describeMismatch(v: A): Option[String]
 
-  override def apply(v: A): MatchResult[A] = if (matches(v)) {
+  def matches[A1 <: A](v: A1): MatchResult[A1] = if (test(v)) {
     Match(v, this)
   } else {
     Mismatch(v, this)
   }
 
-  def &&(other: Matcher[A]): Matcher[A] = new Matcher[A] {
-    override def matches(v: A): Boolean = self.matches(v) && other.matches(v)
+  def &&[A1 <: A](other: Matcher[A1]): Matcher[A1] = new Matcher[A1] {
+    override def test(v: A1): Boolean = self.test(v) && other.test(v)
 
     override def description: String = self.description + " && " + other.description
 
     override def negativeDescription: String = self.negativeDescription + " || " + other.negativeDescription
 
-    override def describeMatch(v: A): Option[String] = self(v) match {
+    override def describeMatch(v: A1): Option[String] = self.matches(v) match {
       case result if result.matches => self.describeMatch(v)
       case _ => None
     }
 
-    override def describeMismatch(v: A): Option[String] = (self.describeMismatch(v), other.describeMismatch(v)) match {
+    override def describeMismatch(v: A1): Option[String] = (self.describeMismatch(v), other.describeMismatch(v)) match {
       case (Some(m1), Some(m2)) => Some(m1 + " || " + m2)
       case (m1@Some(_), None) => m1
       case (None, m2@Some(_)) => m2
@@ -77,24 +77,24 @@ trait Matcher[A] extends (A => MatchResult[A]) {
     }
   }
 
-  def ||(other: Matcher[A]): Matcher[A] = new Matcher[A] {
-    override def matches(v: A): Boolean = self.matches(v) || other.matches(v)
+  def ||[A1 <: A](other: Matcher[A1]): Matcher[A1] = new Matcher[A1] {
+    override def test(v: A1): Boolean = self.test(v) || other.test(v)
 
     override def description: String = self.description + " || " + other.description
 
     override def negativeDescription: String = self.negativeDescription + " && " + other.negativeDescription
 
-    override def describeMatch(v: A): Option[String] = self(v) match {
+    override def describeMatch(v: A1): Option[String] = self.matches(v) match {
       case result if result.matches => self.describeMatch(v)
       case _ => other.describeMatch(v)
     }
 
-    override def describeMismatch(v: A): Option[String] =
+    override def describeMismatch(v: A1): Option[String] =
       self.describeMismatch(v).map(_ + " && ") |+| other.describeMismatch(v)
   }
 
   def unary_!(): Matcher[A] = new Matcher[A] {
-    override def matches(v: A): Boolean = !self.matches(v)
+    override def test(v: A): Boolean = !self.test(v)
 
     override def description: String = self.negativeDescription
 
@@ -106,26 +106,36 @@ trait Matcher[A] extends (A => MatchResult[A]) {
   }
 }
 
-trait Matchers { self =>
+// TODO: clean up
+trait Matchers {
+  self =>
 
+  // TODO: make it a default instance of some Pretifier
   def fmt[A](s: A): String = s match {
     case x: String => "\"" + x + "\""
     case x: Char => s"'$x'"
     case x@(_: Boolean | _: Byte | _: Short | _: Int | _: Long | _: Float | _: Double) => s"$x"
-    case x => s"`$x'"
+//    case x: Map[_, _] => x.map { case (k, v) => fmt(k) + ": " + fmt(v) }.mkString("{", ", ", "}")
+    case x => x.toString
+  }
+
+  def fmtRight[A](s : A): String = s match {
+    case x: Traversable[_] => x.map(fmt).mkString("[", ", ", "]")
   }
 
   trait ImplicitMatchers {
-    implicit class AnyMatcher[A](that: A) {
-      def matches(ms: Matcher[A]): MatchResult[A] = ms(that)
-    }
 
-    implicit def any2matcher[A](x: A): Matcher[A] = is(x)
+    // implicit def any2matcher[A](x: A): Matcher[A] = is(x)
 
     implicit class QueryCollectionSupport[A, +Repr](that: TraversableLike[A, Repr]) {
       def query[That](matcher: Matcher[A])(implicit bf: CanBuildFrom[Repr, MatchResult[A], That]): That =
-        that.map(matcher)
+        that.map(matcher.matches)
     }
+
+    implicit class AnyMatcher[A](that: A) {
+      def matches(matcher: Matcher[A]): MatchResult[A] = matcher.matches(that)
+    }
+
   }
 
   object implicits extends ImplicitMatchers
@@ -139,56 +149,46 @@ trait Matchers { self =>
     case Seq(y, ys@_*) => reduce(x |+| y, ys)
   }
 
-  class FunMatcher[A](fun: A => Boolean,
-                      _description: String,
-                      _negativeDescription: String,
-                      _describeMatch: A => String,
-                      _describeMismatch: A => String) extends Matcher[A] {
-    override def description: String = _description
-
-    override def negativeDescription: String = _negativeDescription
-
+  abstract class AbstractMatcher[-A](val description: String, val negativeDescription: String) extends Matcher[A] {
     override def describeMatch(v: A): Option[String] = {
-      if (fun(v)) {
-        Some(_describeMatch(v))
+      if (test(v)) {
+        Some(fmt(v) + " " + description)
       } else {
         None
       }
     }
 
     override def describeMismatch(v: A): Option[String] = {
-      if (!fun(v)) {
-        Some(_describeMismatch(v))
+      if (!test(v)) {
+        Some(fmt(v) + " " + negativeDescription)
       } else {
         None
       }
     }
+  }
 
-    override def matches(v: A): Boolean = fun(v)
+  class FunMatcher[-A](val fun: A => Boolean, description: String, negativeDescription: String)
+    extends AbstractMatcher[A](description, negativeDescription) {
+
+    override def test(v: A): Boolean = fun(v)
   }
 
   object FunMatcher {
     def apply[A](fun: A => Boolean, description: String, negativeDescription: String): FunMatcher[A] =
-      new FunMatcher[A](
-        fun,
-        description,
-        negativeDescription,
-        v => s"${fmt(v)} $description",
-        v => s"${fmt(v)} $negativeDescription"
-      )
+      new FunMatcher[A](fun, description, negativeDescription)
   }
 
   class PropertyMatcher[A, B](val name: String, private val property: A => B, private val matcher: Matcher[B]) extends Matcher[A] {
 
-    override def description: String = name + " " + matcher.description
+    override def description: String = name + " that " + matcher.description
 
-    override def negativeDescription: String = name + " " + matcher.negativeDescription
+    override def negativeDescription: String = name + " that " + matcher.negativeDescription
 
     override def describeMatch(v: A): Option[String] = matcher.describeMatch(property(v)).map(name + " " + _)
 
     override def describeMismatch(v: A): Option[String] = matcher.describeMismatch(property(v)).map(name + " " + _)
 
-    override def matches(v: A): Boolean = matcher(property(v)).matches
+    override def test(v: A): Boolean = matcher.test(property(v))
   }
 
   object PropertyMatcher {
@@ -222,25 +222,70 @@ trait Matchers { self =>
   }
 
   def is[A](x: A): Matcher[A] =
-    FunMatcher[A](v => v == x, s"== ${fmt(x)}", s"!= ${fmt(x)}")
-
- // def is[A](x: Option[A]): Matcher[Option[A]] =
- //   null
+    FunMatcher[A](v => v == x, s"is ${fmt(x)}", s"is not ${fmt(x)}")
 
   def not[A](x: Matcher[A]): Matcher[A] = !x
 
   def gt[A: Ordering](x: A): Matcher[A] =
-    FunMatcher[A](v => implicitly[Ordering[A]].gt(v, x), s"> ${fmt(x)}", s"< ${fmt(x)}")
+    FunMatcher[A](v => implicitly[Ordering[A]].gt(v, x), s"is > ${fmt(x)}", s"is < ${fmt(x)}")
 
   def lt[A: Ordering](x: A): Matcher[A] =
-    FunMatcher[A](v => implicitly[Ordering[A]].lt(v, x), s"< ${fmt(x)}", s"> ${fmt(x)}")
+    FunMatcher[A](v => implicitly[Ordering[A]].lt(v, x), s"is < ${fmt(x)}", s"is > ${fmt(x)}")
 
   def gteq[A: Ordering](x: A): Matcher[A] =
-    FunMatcher[A](v => implicitly[Ordering[A]].gteq(v, x), s">= ${fmt(x)}", s"<= ${fmt(x)}")
+    FunMatcher[A](v => implicitly[Ordering[A]].gteq(v, x), s"is >= ${fmt(x)}", s"is <= ${fmt(x)}")
 
   def lteq[A: Ordering](x: A): Matcher[A] =
-    FunMatcher[A](v => implicitly[Ordering[A]].lteq(v, x), s"<= ${fmt(x)}", s">= ${fmt(x)}")
+    FunMatcher[A](v => implicitly[Ordering[A]].lteq(v, x), s"is <= ${fmt(x)}", s"is >= ${fmt(x)}")
 
+  // matchers for collections
+  def contains[A, B](x: Matcher[A]): Matcher[B] = {
+    new AbstractMatcher[B](s"contains ${x.description}", s"does not contain ${x.description}") {
+      def exists(v: B, p: A => Boolean): Boolean = v match {
+        case v: Option[A] => v.exists(p)
+        case v: Seq[A] => v.exists(p)
+        case v: Traversable[A] => v.exists(p)
+        case _ => throw new IllegalArgumentException(s"${v.getClass} is not supported")
+      }
+
+      override def test(v: B): Boolean = exists(v, x.test)
+    }
+  }
+
+  // TODO: the B should be constrained, but I did not figure out any reasonable way
+  // that will work with different containers (e.g. Option, Traversable, ...)
+  // Ideally there should be a type class that would provide a the needed
+  // functionality
+  def allOf[A, B](x: A, xs: A*): Matcher[B] = {
+    val all = x +: xs
+
+    new AbstractMatcher[B](s"contains all of ${fmtRight(all)}", s"does not contain all of ${fmtRight(all)}") {
+      def contains(v: B, e: A): Boolean = v match {
+        case v: Option[A] => v.contains(e)
+        case v: Seq[A] => v.contains(e)
+        case v: Traversable[A] => v.exists(_ == e)
+        case _ => throw new IllegalArgumentException(s"${v.getClass} is not supported")
+      }
+
+      def missing(v: B): Seq[A] = all.filterNot(e => contains(v, e))
+
+      override def test(v: B): Boolean = missing(v).isEmpty
+
+      override def describeMismatch(v: B): Option[String] =
+        if (!test(v)) Some(s"${fmt(v)} is missing ${fmtRight(missing(v))}") else None
+    }
+  }
+
+  def isEmpty[A <: {def isEmpty : Boolean}](): Matcher[A] =
+    FunMatcher(v => v.isEmpty, "is empty", "is not empty")
+
+  // TODO: types(anyOf(1,2,3))
+  // TODO: types(only(1,2,3))
+  // TODO: types(inOrderOnly(1,2,3))
+  // TODO: types(inOrder(1,2,3))
+
+  // TODO x, xs
+  // TODO: fmt
   def in[A](xs: A*): Matcher[A] =
     FunMatcher[A](
       v => xs.contains(v),
@@ -263,9 +308,10 @@ trait Matchers { self =>
 }
 
 object Matchers extends Matchers
-/*
-trait RepresentationMatchers extends Matchers {
 
+trait RepresentationMatchers {
+
+  import Matchers._
   /*
     case class Declaration(name: String,
                            kind: String,
@@ -297,8 +343,21 @@ trait RepresentationMatchers extends Matchers {
   def kind[A <: {def kind : String}] =
     new PropertyMatchHelper[A, String]("kind", v => v.kind) with StringMatchHelper[A]
 
+  def declaration[A <: {def declaration: Declaration}](x: Matcher[Declaration], xs: Matcher[Declaration]*): Matcher[A] =
+    PropertyMatcher[A, Declaration]("declaration", v => v.declaration, x, xs)
 
+  def file[A <: {def file: String}](x: Matcher[String], xs: Matcher[String]*): Matcher[A] =
+    PropertyMatcher[A, String]("file", v => v.file, x, xs)
+  def file[A <: {def file: String}] =
+    new PropertyMatchHelper[A, String]("file", v => v.file) with StringMatchHelper[A]
+
+  def location[A <: {def location: Option[Location]}](x: Matcher[Option[Location]], xs: Matcher[Option[Location]]*): Matcher[A] =
+    PropertyMatcher[A, Option[Location]]("location", v => v.location, x, xs)
+  def location[A <: {def location: Option[Location]}] =
+    new PropertyMatchHelper[A, Option[Location]]("location", v => v.location) with OptionMatchHelper[A, Location]
 }
+
+object RepresentationMatchers extends RepresentationMatchers
 
 //
 //
@@ -349,4 +408,3 @@ trait RepresentationMatchers extends Matchers {
 //  xs.filter(x => x.isSynthetic && x.declaration.kind == "def", x.signature.parameterLists.forall(y => y.isImplicit && y.size == 1 && y.params.firstHead.map(_.size == 1).getOrElse(false))
 //  * */
 //}
-*/
--- queries/src/test/scala/cz/cvut/fit/prl/scalaimplicit/queries/DslTest.scala
+++ queries/src/test/scala/cz/cvut/fit/prl/scalaimplicit/queries/DslTest.scala
@@ -1,11 +1,12 @@
 import org.scalatest.{FunSuite, Matchers => STMatchers}
 import cats.syntax.semigroup._
+import cz.cvut.fit.prl.scalaimplicit.core.extractor.representation.Representation.{CallSite, Declaration, Location, Parent}
+import cz.cvut.fit.prl.scalaimplicit.queries.Matchers
 import org.scalatest.matchers.{MatchResult => STMatchResult, Matcher => STMatcher}
 
-class DslTest extends FunSuite with STMatchers {
+class DslTest extends FunSuite with STMatchers with Matchers {
 
   import cz.cvut.fit.prl.scalaimplicit.queries._
-  import cz.cvut.fit.prl.scalaimplicit.queries.Matchers._
   import cz.cvut.fit.prl.scalaimplicit.queries.Matchers.implicits._
 
   trait MatcherTesting {
@@ -21,6 +22,7 @@ class DslTest extends FunSuite with STMatchers {
             s"$left ${if (matches) "" else "mis"}matched ${left.reason}"
           )
         } else {
+          // TODO: on of this is wrong - should be mismatched
           STMatchResult(
             matches = false,
             s"$left ${if (matches) "" else "mis"}matched ${left.reason}",
@@ -43,6 +45,21 @@ class DslTest extends FunSuite with STMatchers {
 
   import MatcherTesting._
 
+  class X {
+    def x = 1
+  }
+
+  class Y extends X {
+    def y = 2
+  }
+
+  val x1 = new X
+  val y1 = new Y
+
+  val lx: List[X] = x1 :: y1 :: Nil
+  val ly: List[Y] = y1 :: Nil
+
+
   test("is matcher matches an instance of ") {
     (2 matches is(2)) should matched
     (1 matches is(2)) should mismatched
@@ -50,31 +67,103 @@ class DslTest extends FunSuite with STMatchers {
 
   test("combine matchers using default combinator AND") {
     val c = is(2) |+| is(1)
-    c.description shouldBe "== 2 && == 1"
-    c.negativeDescription shouldBe "!= 2 || != 1"
+    c.description shouldBe "is 2 && is 1"
+    c.negativeDescription shouldBe "is not 2 || is not 1"
 
-    (2 matches c) should mismatched("2 != 1")
-    (1 matches c) should mismatched("1 != 2")
+    (2 matches c) should mismatched("2 is not 1")
+    (1 matches c) should mismatched("1 is not 2")
   }
 
   test("negate matcher") {
-    (1 matches !is(2)) should matched("1 != 2")
+    (1 matches !is(2)) should matched("1 is not 2")
   }
 
   test("negate negated matcher") {
     //noinspection DoubleNegationScala
-    (1 matches !(!is(1))) should matched("1 == 1")
+    (1 matches !(!is(1))) should matched("1 is 1")
   }
 
   test("combine matchers using OR") {
     val c = is(1) || is(2) || is(3)
-    c.description shouldBe "== 1 || == 2 || == 3"
-    c.negativeDescription shouldBe "!= 1 && != 2 && != 3"
+    c.description shouldBe "is 1 || is 2 || is 3"
+    c.negativeDescription shouldBe "is not 1 && is not 2 && is not 3"
 
-    (1 matches c) should matched("1 == 1")
-    (2 matches c) should matched("2 == 2")
-    (3 matches c) should matched("3 == 3")
-    (4 matches c) should mismatched("4 != 1 && 4 != 2 && 4 != 3")
+    (1 matches c) should matched("1 is 1")
+    (2 matches c) should matched("2 is 2")
+    (3 matches c) should matched("3 is 3")
+    (4 matches c) should mismatched("4 is not 1 && 4 is not 2 && 4 is not 3")
+  }
+
+  test("plymorphism") {
+    val mx = FunMatcher[X](v => v.x == 1, "", "")
+    val my = FunMatcher[Y](v => v.y == 2, "", "")
+
+    (x1 matches mx) should matched
+    (y1 matches mx) should matched
+    (y1 matches my) should matched
+    assertDoesNotCompile("x1 matches my")
+
+    val mlx = FunMatcher[List[X]](v => v.head.x == 1, "", "")
+    val mly = FunMatcher[List[Y]](v => v.head.y == 2, "", "")
+
+    (lx matches mlx) should matched
+    (ly matches mlx) should matched
+    (ly matches mly) should matched
+    assertDoesNotCompile("lx matches mly")
+  }
+
+  test("Option") {
+    //  (None matches allOf(2)) should mismatched("is empty")
+
+    //    def matches[A](what: A, mather: Matcher[A]) = mather.matches(what)
+    //
+    //    matches[Container[Int]](Option(1), isEmpty())
+    //
+    //    matches(Option(1), isEmpty())
+    //
+    //    isEmpty[Int]().apply(Option(1))
+    //
+    //    Option(1) matches(isEmpty())
+
+    //isEmpty().apply(Option(null))
+    //(Option(null) matches isEmpty()) should matched("`None' is empty")
+  }
+
+  test("isEmpty - option") {
+    (Option(1) matches isEmpty()) should mismatched("Some(1) is not empty")
+    (Option(null) matches isEmpty()) should matched("None is empty")
+    (None matches isEmpty()) should matched("None is empty")
+  }
+
+  test("isEmpty - sequences") {
+    (Seq(1) matches isEmpty()) should mismatched("List(1) is not empty")
+    (Nil matches isEmpty()) should matched("List() is empty")
+    (List() matches isEmpty()) should matched("List() is empty")
+  }
+
+  test("isEmpty - map") {
+    (Map(1 -> 2, 2 -> 3) matches isEmpty()) should mismatched("Map(1 -> 2, 2 -> 3) is not empty")
+  }
+
+  test("allOf - sequence") {
+    (lx matches allOf(x1)) should matched
+    (lx matches allOf(y1)) should matched
+
+    val mx1: Matcher[List[X]] = allOf(y1)
+    (lx matches mx1) should matched
+
+    (Seq(1) matches allOf(1)) should matched("List(1) contains all of [1]")
+    (Seq(1, 2) matches allOf(1, 2, 3, 4)) should mismatched("List(1, 2) is missing [3, 4]")
+  }
+
+  test("allOf -  option") {
+    (Option(1) matches allOf(1)) should matched("Some(1) contains all of [1]")
+    (None matches allOf(1)) should mismatched("None is missing [1]")
+  }
+
+  test("allOf - map") {
+    (Map(1 -> 2) matches allOf(1 -> 2)) should matched("Map(1 -> 2) contains all of [(1,2)]")
+    (Map(1 -> 2) matches allOf(1 -> 3)) should mismatched("Map(1 -> 2) is missing [(1,3)]")
   }
 
   //  test("in") {
@@ -115,22 +204,44 @@ class DslTest extends FunSuite with STMatchers {
   //
   //  }
   //
-  //  test("property matcher - size") {
-  //    val x = 1 :: 2 :: Nil
-  //    val m: Matcher[List[Int]] = queries.Matchers.size(is(2))
-  //
-  //    m matches x
-  //    m.describeMatch(x) should contain ("size `2' == `2'")
-  //    m.describeMatch(3 :: x) shouldBe empty
-  //    m.describeMismatch(3 :: x) should contain ("size `3' != `2'")
-  //  }
+  test("property matcher - size") {
+    val x = 1 :: 2 :: Nil
+    val m: Matcher[List[Int]] = Matchers.size(is(2))
+
+    (x matches Matchers.size(is(2))) should matched("size 2 is 2")
+    (Set(1,2) matches size(is(2))) should matched("size 2 is 2")
+  }
+
 
   test("collection matching") {
     val q = is(2) || is(4)
     val col = Seq(1, 2, 3, 4)
 
-   (col query q) should contain theSameElementsInOrderAs Seq(Mismatch(1, q), Match(2, q), Mismatch(3, q), Match(4, q))
-   (col query q) map (_.toOption) should contain theSameElementsInOrderAs Seq(None, Some(2), None, Some(4))
-   (col query q) map (_.toEither) should contain theSameElementsInOrderAs Seq(Left("1 != 2 && 1 != 4"), Right(2), Left("3 != 2 && 3 != 4"), Right(4))
+    (col query q) should contain theSameElementsInOrderAs Seq(Mismatch(1, q), Match(2, q), Mismatch(3, q), Match(4, q))
+    (col query q) map (_.toOption) should contain theSameElementsInOrderAs Seq(None, Some(2), None, Some(4))
+    (col query q) map (_.toEither) should contain theSameElementsInOrderAs Seq(Left("1 is not 2 && 1 is not 4"), Right(2), Left("3 is not 2 && 3 is not 4"), Right(4))
+  }
+
+  test("callsite test") {
+
+    import RepresentationMatchers._
+
+    val cs = CallSite(
+      "name-1",
+      "code",
+      Some(Location("file", 1, 2)),
+      true,
+      Declaration("decl-name", "def", Some(Location("fil",3,4)), true, None, Seq(Parent("parent", null, null))),
+      null,
+      null
+    )
+
+    cs.matches(
+      name(startsWith("name")) &&
+      declaration(
+        kind(in("def", "val")) && location(contains(file(startsWith("file"))))
+      )
+    ) should mismatched("declaration location Some(fil:3:4) does not contain file that starts with \"file\"")
+
   }
 }
